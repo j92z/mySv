36e6e39fbfbace326594c618a2eac4bccdf10e92
cozzz <zeco11320@163.com>
2019-02-20 09:49:18 +0800

    chang route

--- src/Core/Route.php
+++ src/Core/Route.php
@@ -31,6 +31,17 @@ class Route
             return;
         }
         $path_map = explode('/', trim($request->request()->server['path_info'], '/'));
+        $path_length = count($path_map);
+        if ($path_length > 2) {
+            $this->namespace = '';
+            for ($i = 0; $i < $path_length - 2; ++$i) {
+                if (empty($this->namespace)) {
+                    $this->namespace .= ucfirst($path_map[$i]);
+                } else {
+                    $this->namespace .= '\\'.$path_map[$i];
+                }
+            }
+        }
         $c = isset($path_map[0]) && !empty($path_map[0]) && $path_map[0] != '/' ? $path_map[0] : 'index';
         $a = isset($path_map[1]) && !empty($path_map[1]) ? $path_map[1] : 'index';
         $class = $this->namespace.'\\'.$this->controller.'\\'.ucfirst($c);
